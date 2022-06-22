c4b7be6e46cda13370cdbc1f1a6240dc20118d34
Fly <Fly@qq.com>
2022-06-22 15:25:11 +0800

    git commit1

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author zzfstart
+ * @create 2022-06-22-15:24
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+    }
+}
